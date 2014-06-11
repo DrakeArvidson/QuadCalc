8225f3a276668dfd9edb40083b91cecb719c954f
Drake Arvidson <darvidson@denniskirk.com>
2014-06-11 16:22:32 -0500

    Added more functionality to handle the NaN(not a number) results. Instead it will display only the real X or say that the answer is that X is an imaginary number.

--- src/background/Quadratic.java
+++ src/background/Quadratic.java
@@ -17,13 +17,26 @@ public class Quadratic {
         
         double ans1 = (-b + (Math.sqrt(Math.pow(b, 2) - 4*a*c)))/(2*a);
         double ans2 = (-b - (Math.sqrt(Math.pow(b, 2) - 4*a*c)))/(2*a);
-        result = result + "X = " + ans1 + "\n or X = " + ans2;
+        if (Double.isNaN(ans1) && Double.isNaN(ans2)) {
+        	result = result + "X is an imaginary number";
+        }
         
+        else if (Double.isNaN(ans1) && !Double.isNaN(ans2)) {
+        	result = result + "X = " + ans2;
+        }
+        
+        else if (Double.isNaN(ans2) && !Double.isNaN(ans1)) {
+        	result = result + "X = " + ans1;
+        }
+        
+        else {
+        	result = result + "X = " + ans1 + "\n or X = " + ans2;
+        }
         return result;
     }
     
-    public static void main(String[] args) {
-        Quadratic quad = new Quadratic(2, 3, 1);
-        System.out.println(quad.solve());
-    }
+//    public static void main(String[] args) {
+//        Quadratic quad = new Quadratic(2, 3, 1);			This was only here to test the Quadratic function.
+//        System.out.println(quad.solve());
+//    }
 }
